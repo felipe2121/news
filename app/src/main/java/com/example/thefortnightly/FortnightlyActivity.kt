3b5012a09d1f9d7bb7adef29e52f414ab00b147b
felipeholanda <felipe.holanda12@yahoo.com.br>
2020-08-14 17:26:59 -0300

    Basic

--- app/src/main/java/com/example/thefortnightly/FortnightlyActivity.kt
+++ app/src/main/java/com/example/thefortnightly/FortnightlyActivity.kt
@@ -1,7 +1,10 @@
 package com.example.thefortnightly
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_fortnightly.*
 
 class FortnightlyActivity : AppCompatActivity() {
 
@@ -9,5 +12,47 @@ class FortnightlyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fortnightly)
 
+        setUpUI()
+    }
+
+    private fun setUpUI() {
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val toogle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        toogle.syncState()
+
+        drawer.addDrawerListener(toogle)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
