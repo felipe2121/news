440f42e228ca42cdb138961cf00f2217b0dfcc1f
Felipe Holanda <felipeholanda@Mac-mini-de-NATI.local>
2020-08-14 13:48:43 -0300

    commit inicial

---
+++ app/src/main/java/com/example/thefortnightly/FortnightlyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.thefortnightly
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FortnightlyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fortnightly)
+
+    }
+}
