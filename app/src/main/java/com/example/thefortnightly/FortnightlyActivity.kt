059166302322a59a8210f89238d6651334cb5214
felipeholanda <felipe.holanda12@yahoo.com.br>
2020-08-17 14:02:30 -0300

    Basic

--- app/src/main/java/com/example/thefortnightly/FortnightlyActivity.kt
+++ app/src/main/java/com/example/thefortnightly/FortnightlyActivity.kt
@@ -15,6 +15,30 @@ class FortnightlyActivity : AppCompatActivity() {
         setUpUI()
     }
 
+    /*override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }*/
+
     private fun setUpUI() {
 
         setSupportActionBar(toolbar)
